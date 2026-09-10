ca30cdf19133a36d2fb9c8da532b847d54d11b88
Guy Blanchfield <guyblanchfield@gmail.com>
2026-09-10 20:14:47 +0100

    Gulp html, js and css processing and added browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -10,20 +10,27 @@
 
 // what do we need?
 
-// gulp-uglify, gulp-concat, gulp-css, gulp-terser? gulp-html-minifier-terser, gulp-sourcemaps?
+// gulp-uglify, gulp-concat, gulp-css, gulp-terser, gulp-html-minifier-terser, gulp-sourcemaps?
 
-// do we need a src folder and a build folder - yes. To get this to work we need to add the following to settings.json
-// "liveServer.settings.root": "/src",
-// NB!!!! This will apply to ALL projects though. Maybe think about using browsersync for projects like this
+// NB! If we're only using autoprefixer, then gulp-autoprefixer will do
+// but if we want any other PostCSS tools then use gulp-postcss with autoprefixer etc as plugins.
 
-// declare vars
+// We need a src folder and a dist folder. To get this to work we need to use browsersync instead of liveserver
 
+// -------------------------------------------------------------------------------------------------------------
+
+// Declare vars
+
+const browsersync = require("browser-sync").create();
 const { src, dest, watch, series } = require("gulp");
 
 const concat = require("gulp-concat");
 const cssmin = require("gulp-css");
 const terser = require("gulp-terser");
 const htmlmin = require("gulp-html-minifier-terser");
+// NB! we need the .default on the autoprefixer require otherwise it errors
+// see https://stackoverflow.com/questions/79659584/autoprefixer-is-not-a-function
+const prefix = require("gulp-autoprefixer").default;
 // const sourcemaps = require("gulp-sourcemaps");
 
 // JS Task
@@ -44,8 +51,24 @@ function minifyJS() {
 
 // CSS Task
 // we actually want to concatenate all the css, then minify it
-function minifyCSS() {
-	return src("src/css/*.css").pipe(cssmin()).pipe(dest("dist/css"));
+
+// we need to get the css files in the right order so let's have an array of the files instead of a glob
+// look into postcss to do this better
+const cssfiles = [
+	"src/css/reset.css",
+	"src/css/global.css",
+	"src/css/header.css",
+	"src/css/hero.css",
+	"src/css/main.css",
+	"src/css/home.css",
+	"src/css/footer.css",
+	"src/css/googlemap.css",
+];
+
+// concatenate, minify and autoprefix the css
+function processCSS() {
+	// prefix() should use the browserslist specified in package.json
+	return src(cssfiles).pipe(concat("styles.css")).pipe(cssmin()).pipe(prefix()).pipe(dest("dist/css"));
 }
 
 // HTML Task
@@ -55,9 +78,31 @@ function minifyHTML() {
 		.pipe(dest("dist"));
 }
 
-// function defaultTask(cb) {
-// 	// default task code here
-// 	cb();
-// }
+// Browsersync Tasks
+function browsersyncServe(cb) {
+	browsersync.init({
+		server: {
+			baseDir: "dist",
+		},
+	});
 
-exports.default = series(minifyJS, minifyCSS, minifyHTML);
+	cb();
+}
+
+function browsersyncReload(cb) {
+	browsersync.reload();
+	cb();
+}
+
+// Watch Task
+function watchTask() {
+	// two different functions
+	// one for just reloading the browser
+
+	watch("src/**/*.html", browsersyncReload);
+
+	// one for building, then refreshing (not sure we'll need this as distinct from running gulp, but go with it for now)
+	watch(["src/css/*.css", "src/js/*.js"], series(minifyJS, browsersyncReload));
+}
+
+exports.default = series(minifyJS, processCSS, minifyHTML, browsersyncServe);
